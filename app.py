67dcd2b8379a30f8a7561f18f265d1d2549524ce
Vibhor Bhatt <108974586+vibhorbhatt777@users.noreply.github.com>
2025-02-14 09:36:21 +0000

    added the flask project

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")#part of the url after domain name
 def hello():
-     return "<p> Hello world </p>"
+     return "<p> Hello Vibhor </p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0' , debug=True)
   
+
